cbc8cb6c5571cd269c06d2b3c78e089f76291453
Harish <harishsanthoshkumar20@gmail.com>
2025-12-29 17:37:11 +0530

    C Programming

--- level_4/level4_code_13.c
+++ level_4/level4_code_13.c
@@ -2,20 +2,18 @@
 
 int main()
 {
-    int n, a, last, digits = 1, middle;
+    int n, a, last, d= 1, mid;
     scanf("%d", &n);
-
     a = n;
     last = n % 10;
 loop:
     if (a >= 10)
     {
         a = a / 10;
-        digits = digits * 10;
+        d = d * 10;
         goto loop;
     }
-    
-    middle = (n % digits) / 10;
-    n = last * digits + middle * 10 + a;
+    mid = (n % d) / 10;
+    n = last * d + mid * 10 + a;
     printf("%d", n);
 }
