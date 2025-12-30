9da2102b7639b972cc86495f57918c372b53b7bb
Harish S <harishsanthoshkumar20@gmail.com>
2025-12-30 08:48:15 +0530

    Delete Level1 directory

--- Level1/level1_code_16.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a;
-    scanf("%d", &a);
-    a=a-(((a/10) % 2) * 5);
-    printf("%d\n",a);
-    return 0;
-}
--- Level1/level1_code_5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    scanf("%d",&a);
-    b=a%8;
-    printf("%d",b);
-
-}
